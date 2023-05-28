2b6a89011f6be11718a5c2ce0dca5903661f5d34
Ryan McPeck <mcoso.dev@gmail.com>
2023-05-28 13:19:35 -0400

    chore: linting

--- contracts/EIPandaAuction.sol
+++ contracts/EIPandaAuction.sol
@@ -21,104 +21,114 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract EIPandaAuction is ReentrancyGuard, Ownable {
-    struct Auction {
-        uint256 tokenId;
-        uint256 endTime;
-        uint256 highestBid;
-        address highestBidder;
+  struct Auction {
+    uint256 tokenId;
+    uint256 endTime;
+    uint256 highestBid;
+    address highestBidder;
+  }
+
+  IERC721 public eipandas;
+  IERC20 public lidoStETH;
+  uint256 public constant RESERVE_PRICE = 0.01 ether;
+  Auction public currentAuction;
+  bool public auctionInProgress;
+
+  // The minimum percentage difference between the last bid amount and the current bid
+  uint8 public minBidIncrementPercentage;
+
+  event PandaDeposited(uint256 tokenId);
+  event AuctionStarted(uint256 tokenId, uint256 endTime);
+  event AuctionSuccessful(uint256 tokenId, address indexed buyer, uint256 totalPrice);
+  event PandaKilled(uint256 tokenId);
+
+  constructor(address _eipandasAddress, address _lidoStETHAddress) {
+    eipandas = IERC721(_eipandasAddress);
+    lidoStETH = IERC20(_lidoStETHAddress);
+  }
+
+  modifier activeAuction() {
+    require(auctionInProgress, "There is no active auction");
+    _;
+  }
+
+  modifier noActiveAuction() {
+    require(!auctionInProgress, "An auction is already in progress");
+    _;
+  }
+
+  function depositPanda(uint256 tokenId) external nonReentrant {
+    require(eipandas.ownerOf(tokenId) == msg.sender, "Ser, this is not your panda");
+
+    eipandas.safeTransferFrom(msg.sender, address(this), tokenId);
+    emit PandaDeposited(tokenId);
+  }
+
+  function startAuction(uint256 tokenId) external nonReentrant noActiveAuction {
+    require(eipandas.ownerOf(tokenId) == address(this), "Token not deposited");
+
+    uint256 auctionEndTime = block.timestamp + 1 days;
+    currentAuction = Auction({
+      tokenId: tokenId,
+      endTime: auctionEndTime,
+      highestBidder: address(0),
+      highestBid: 0
+    });
+
+    auctionInProgress = true;
+
+    emit AuctionStarted(tokenId, auctionEndTime);
+  }
+
+  function placeBid(uint256 bidAmount) external nonReentrant activeAuction {
+    require(block.timestamp < currentAuction.endTime, "Auction has ended");
+    require(bidAmount >= RESERVE_PRICE, "Bid amount less than reserve");
+    require(bidAmount > currentAuction.highestBid, "Bid amount less than current bid");
+
+    // Transfer the stETH tokens from the bidder to the contract
+    lidoStETH.transferFrom(msg.sender, address(this), bidAmount);
+
+    if (currentAuction.highestBidder != address(0)) {
+      // Transfer the previous highest bid back to the bidder
+      lidoStETH.transfer(currentAuction.highestBidder, currentAuction.highestBid);
     }
 
-    IERC721 public eipandas;
-    IERC20 public lidoStETH;
-    uint256 public constant RESERVE_PRICE = 0.01 ether;
-    Auction public currentAuction;
-    bool public auctionInProgress;
-    
-    // The minimum percentage difference between the last bid amount and the current bid
-    uint8 public minBidIncrementPercentage;
+    currentAuction.highestBid = bidAmount;
+    currentAuction.highestBidder = msg.sender;
+  }
 
-    event PandaDeposited(uint256 tokenId);
-    event AuctionStarted(uint256 tokenId, uint256 endTime);
-    event AuctionSuccessful(uint256 tokenId, address indexed buyer, uint256 totalPrice);
-    event PandaKilled(uint256 tokenId);
+  function endAuction() external nonReentrant activeAuction {
+    require(block.timestamp >= currentAuction.endTime, "Auction not yet ended");
 
-    constructor(address _eipandasAddress, address _lidoStETHAddress) {
-        eipandas = IERC721(_eipandasAddress);
-        lidoStETH = IERC20(_lidoStETHAddress);
+    if (currentAuction.highestBid >= RESERVE_PRICE) {
+      // Transfer the token to the highest bidder
+      eipandas.safeTransferFrom(
+        address(this),
+        currentAuction.highestBidder,
+        currentAuction.tokenId
+      );
+      emit AuctionSuccessful(
+        currentAuction.tokenId,
+        currentAuction.highestBidder,
+        currentAuction.highestBid
+      );
+    } else {
+      // R.I.P.
+      eipandas.safeTransferFrom(
+        address(this),
+        address(0x000000000000000000000000000000000000dEaD),
+        currentAuction.tokenId
+      );
+      emit PandaKilled(currentAuction.tokenId);
     }
 
-    modifier activeAuction() {
-        require(auctionInProgress, "There is no active auction");
-        _;
-    }
+    // Clear auction details
+    delete currentAuction;
+    auctionInProgress = false;
+  }
 
-    modifier noActiveAuction() {
-        require(!auctionInProgress, "An auction is already in progress");
-        _;
-    }
-    
-
-    function depositPanda(uint256 tokenId) external nonReentrant {
-        require(eipandas.ownerOf(tokenId) == msg.sender, "Ser, this is not your panda");
-    
-        eipandas.safeTransferFrom(msg.sender, address(this), tokenId);
-        emit PandaDeposited(tokenId);
-    }
-
-    function startAuction(uint256 tokenId) external nonReentrant noActiveAuction {
-        require(eipandas.ownerOf(tokenId) == address(this), "Token not deposited");
-        
-        uint256 auctionEndTime = block.timestamp + 1 days;
-        currentAuction = Auction({
-            tokenId: tokenId,
-            endTime: auctionEndTime,
-            highestBidder: address(0),
-            highestBid: 0
-        });
-
-        auctionInProgress = true;
-
-        emit AuctionStarted(tokenId, auctionEndTime);
-    }
-
-    function placeBid(uint256 bidAmount) external nonReentrant activeAuction {
-        require(block.timestamp < currentAuction.endTime, "Auction has ended");
-        require(bidAmount >= RESERVE_PRICE, "Bid amount less than reserve");
-        require(bidAmount > currentAuction.highestBid, "Bid amount less than current bid");
-
-        // Transfer the stETH tokens from the bidder to the contract
-        lidoStETH.transferFrom(msg.sender, address(this), bidAmount);
-
-        if (currentAuction.highestBidder != address(0)) {
-            // Transfer the previous highest bid back to the bidder
-            lidoStETH.transfer(currentAuction.highestBidder, currentAuction.highestBid);
-        }
-
-        currentAuction.highestBid = bidAmount;
-        currentAuction.highestBidder = msg.sender;
-    }
-
-
-    function endAuction() external nonReentrant activeAuction {
-        require(block.timestamp >= currentAuction.endTime, "Auction not yet ended");
-
-        if (currentAuction.highestBid >= RESERVE_PRICE) {
-            // Transfer the token to the highest bidder
-            eipandas.safeTransferFrom(address(this), currentAuction.highestBidder, currentAuction.tokenId);
-            emit AuctionSuccessful(currentAuction.tokenId, currentAuction.highestBidder, currentAuction.highestBid);
-        } else {
-            // R.I.P.
-            eipandas.safeTransferFrom(address(this), address(0x000000000000000000000000000000000000dEaD), currentAuction.tokenId);
-            emit PandaKilled(currentAuction.tokenId);
-        }
-
-        // Clear auction details
-        delete currentAuction;
-        auctionInProgress = false;
-    }
-
-    function withdrawStETH() external nonReentrant onlyOwner {
-        uint256 stethBalance = lidoStETH.balanceOf(address(this));
-        lidoStETH.transfer(owner(), stethBalance);
-    }
+  function withdrawStETH() external nonReentrant onlyOwner {
+    uint256 stethBalance = lidoStETH.balanceOf(address(this));
+    lidoStETH.transfer(owner(), stethBalance);
+  }
 }
--- contracts/EIPandaAuctionHouse.sol
+++ contracts/EIPandaAuctionHouse.sol
@@ -29,239 +29,246 @@ import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
 // found here: https://github.com/lidofinance/lido-dao/blob/cadffa46a2b8ed6cfa1127fca2468bae1a82d6bf/contracts/0.8.9/WithdrawalQueue.sol#LL125C57-L125C77
 interface ILidoWithdrawalQueue {
-    function requestWithdrawals(uint256[] calldata _amounts, address _owner) external returns (uint256[] memory requestIds);
+  function requestWithdrawals(uint256[] calldata _amounts, address _owner)
+    external
+    returns (uint256[] memory requestIds);
 }
 
 contract EIPandaAuctionHouse is ReentrancyGuard, Ownable {
+  struct Auction {
+    uint256 pandaId;
+    uint256 startTime;
+    uint256 endTime;
+    uint256 highestBid;
+    address highestBidder;
+  }
 
-    struct Auction {
-        uint256 pandaId;
-        uint256 startTime;
-        uint256 endTime;
-        uint256 highestBid;
-        address highestBidder;
+  // The EIPandas NFT contract
+  IERC721 public eipandas = IERC721(0xA09129080eD12CF1B1C7a6e723C63E0820E9D3ae);
+
+  // The Lido stETH token contract
+  IERC20 public lidoStETH = IERC20(0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84);
+
+  // Lido withdraw contract
+  ILidoWithdrawalQueue public lidoWithdrawalQueue =
+    ILidoWithdrawalQueue(0x889edC2eDab5f40e902b864aD4d7AdE8E412F9B1);
+
+  // The minimum amount of time left in an auction after a new bid is created
+  uint256 public timeBuffer = 1200; // 20 minutes
+
+  // The minimum price accepted in an auction
+  uint256 public reservePrice = 0.01 ether;
+
+  // The minimum percentage difference between the last bid amount and the current bid
+  uint8 public minBidIncrementPercentage = 10; // 10%
+
+  // The duration of a single auction
+  uint256 public duration = 1 days;
+
+  // The current auction
+  Auction public auction;
+
+  // active auction flag
+  bool public auctionInProgress;
+
+  // array of pandas to be auctioned
+  uint256[] public pandasToAuction;
+
+  event PandaDeposited(uint256 tokenId);
+  event AuctionStarted(uint256 tokenId, uint256 endTime);
+  event AuctionSuccessful(uint256 tokenId, address indexed buyer, uint256 totalPrice);
+  event PandaKilled(uint256 tokenId);
+  event AuctionCreated(uint256 indexed nounId, uint256 startTime, uint256 endTime);
+  event AuctionBid(uint256 indexed nounId, address sender, uint256 value, bool extended);
+  event AuctionExtended(uint256 indexed nounId, uint256 endTime);
+  event AuctionSettled(uint256 indexed nounId, address winner, uint256 amount);
+  event AuctionTimeBufferUpdated(uint256 timeBuffer);
+  event AuctionReservePriceUpdated(uint256 reservePrice);
+  event AuctionMinBidIncrementPercentageUpdated(uint256 minBidIncrementPercentage);
+
+  constructor(address _eipandasAddress, address _lidoStETHAddress) {
+    eipandas = IERC721(_eipandasAddress);
+    lidoStETH = IERC20(_lidoStETHAddress);
+  }
+
+  modifier activeAuction() {
+    require(auctionInProgress, "There is no active auction");
+    _;
+  }
+
+  modifier noActiveAuction() {
+    require(!auctionInProgress, "An auction is already in progress");
+    _;
+  }
+
+  /**
+   * @notice Settle the current auction, fetch a new panda, and put it up for auction.
+   */
+  function settleCurrentAndCreateNewAuction() external nonReentrant activeAuction {
+    _settleAuction();
+    _startAuction();
+  }
+
+  /**
+   * @notice Place a bid to save a Panda, with a given amount.
+   * @dev This contract only accepts payment in stETH.
+   */
+
+  function placeBid(uint256 _bidAmount) external nonReentrant activeAuction {
+    Auction memory _auction = auction;
+
+    require(block.timestamp < _auction.endTime, "Auction expired");
+    require(_bidAmount >= reservePrice, "Must send at least reservePrice");
+    require(
+      _bidAmount >= _auction.highestBid + ((_auction.highestBid * minBidIncrementPercentage) / 100),
+      "Must send more than last bid by minBidIncrementPercentage amount"
+    );
+
+    // Transfer the stETH tokens from the bidder to the contract
+    require(
+      lidoStETH.transferFrom(msg.sender, address(this), _bidAmount),
+      "Insufficent stETH or approval not set"
+    );
+
+    address lastBidder_ = _auction.highestBidder;
+
+    // Refund the last bidder, if applicable
+    if (lastBidder_ != address(0)) {
+      // Transfer the previous highest bid back to the bidder
+      lidoStETH.transfer(lastBidder_, _auction.highestBid);
     }
 
-    // The EIPandas NFT contract
-    IERC721 public eipandas = IERC721(0xA09129080eD12CF1B1C7a6e723C63E0820E9D3ae);
+    auction.highestBid = _bidAmount;
+    auction.highestBidder = msg.sender;
 
-    // The Lido stETH token contract
-    IERC20 public lidoStETH = IERC20(0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84);
-
-    // Lido withdraw contract
-    ILidoWithdrawalQueue public lidoWithdrawalQueue = ILidoWithdrawalQueue(0x889edC2eDab5f40e902b864aD4d7AdE8E412F9B1);
-
-    // The minimum amount of time left in an auction after a new bid is created
-    uint256 public timeBuffer = 1200; // 20 minutes
-
-    // The minimum price accepted in an auction
-    uint256 public reservePrice = 0.01 ether;
-
-    // The minimum percentage difference between the last bid amount and the current bid
-    uint8 public minBidIncrementPercentage = 10; // 10%
-
-    // The duration of a single auction
-    uint256 public duration = 1 days;
-
-    // The current auction
-    Auction public auction;
-
-    // active auction flag
-    bool public auctionInProgress;
-
-    // array of pandas to be auctioned
-    uint256[] public pandasToAuction;
-
-    event PandaDeposited(uint256 tokenId);
-    event AuctionStarted(uint256 tokenId, uint256 endTime);
-    event AuctionSuccessful(uint256 tokenId, address indexed buyer, uint256 totalPrice);
-    event PandaKilled(uint256 tokenId);
-    event AuctionCreated(uint256 indexed nounId, uint256 startTime, uint256 endTime);
-    event AuctionBid(uint256 indexed nounId, address sender, uint256 value, bool extended);
-    event AuctionExtended(uint256 indexed nounId, uint256 endTime);
-    event AuctionSettled(uint256 indexed nounId, address winner, uint256 amount);
-    event AuctionTimeBufferUpdated(uint256 timeBuffer);
-    event AuctionReservePriceUpdated(uint256 reservePrice);
-    event AuctionMinBidIncrementPercentageUpdated(uint256 minBidIncrementPercentage);
-
-    constructor(address _eipandasAddress, address _lidoStETHAddress) {
-        eipandas = IERC721(_eipandasAddress);
-        lidoStETH = IERC20(_lidoStETHAddress);
+    // Extend the auction if the bid was received within `timeBuffer` of the auction end time
+    bool extended = _auction.endTime - block.timestamp < timeBuffer;
+    if (extended) {
+      auction.endTime = _auction.endTime = block.timestamp + timeBuffer;
     }
 
-    modifier activeAuction() {
-        require(auctionInProgress, "There is no active auction");
-        _;
+    emit AuctionBid(_auction.pandaId, auction.highestBidder, auction.highestBid, extended);
+
+    if (extended) {
+      emit AuctionExtended(_auction.pandaId, _auction.endTime);
+    }
+  }
+
+  /**
+   * @notice Set the auction time buffer.
+   * @dev Only callable by the owner.
+   */
+  function setTimeBuffer(uint256 _timeBuffer) external onlyOwner {
+    timeBuffer = _timeBuffer;
+
+    emit AuctionTimeBufferUpdated(_timeBuffer);
+  }
+
+  /**
+   * @notice Set the auction reserve price.
+   * @dev Only callable by the owner.
+   */
+  function setReservePrice(uint256 _reservePrice) external onlyOwner {
+    reservePrice = _reservePrice;
+
+    emit AuctionReservePriceUpdated(_reservePrice);
+  }
+
+  /**
+   * @notice Set the auction minimum bid increment percentage.
+   * @dev Only callable by the owner.
+   */
+  function setMinBidIncrementPercentage(uint8 _minBidIncrementPercentage) external onlyOwner {
+    minBidIncrementPercentage = _minBidIncrementPercentage;
+
+    emit AuctionMinBidIncrementPercentageUpdated(_minBidIncrementPercentage);
+  }
+
+  /**
+   * @notice Set the auction duration time.
+   * @dev Only callable by the owner.
+   */
+  function setDuration(uint256 _duration) external onlyOwner {
+    duration = _duration;
+  }
+
+  function pandasToSave() external view returns (uint256) {
+    return pandasToAuction.length;
+  }
+
+  function depositPandas(uint256[] calldata _tokenIds) external nonReentrant {
+    for (uint256 i = 0; i < _tokenIds.length; i++) {
+      _depositPanda(_tokenIds[i]);
+    }
+  }
+
+  function _depositPanda(uint256 _tokenId) internal {
+    require(eipandas.ownerOf(_tokenId) == msg.sender, "Ser, this is not your panda");
+    eipandas.safeTransferFrom(msg.sender, address(this), _tokenId);
+    pandasToAuction.push(_tokenId);
+    emit PandaDeposited(_tokenId);
+  }
+
+  /**
+   * @notice Create an auction.
+   * @dev Store the auction details in the `auction` state variable and emit an AuctionCreated event.
+   * Check to make sure there are pandas to auction. Start new auction with panda using LIFO
+   */
+  function _startAuction() internal nonReentrant noActiveAuction {
+    // check if there are any pandas to auction
+    uint256 numPandasToAuction_ = pandasToAuction.length;
+    require(numPandasToAuction_ > 0, "No pandas to save at this time");
+
+    // grab a panda from the array using LIFO
+    uint256 pandaId_ = pandasToAuction[numPandasToAuction_ - 1];
+    pandasToAuction.pop();
+
+    uint256 startTime_ = block.timestamp;
+    uint256 endTime_ = startTime_ + duration;
+
+    auction = Auction({
+      pandaId: pandaId_,
+      startTime: startTime_,
+      endTime: endTime_,
+      highestBid: 0,
+      highestBidder: address(0)
+    });
+
+    auctionInProgress = true;
+
+    emit AuctionCreated(pandaId_, startTime_, endTime_);
+  }
+
+  /**
+   * @notice Settle an auction, finalizing the bid and paying out to the owner.
+   * @dev If there are no bids, the Panda is burned.
+   */
+  function _settleAuction() internal nonReentrant activeAuction {
+    Auction memory _auction = auction;
+
+    require(block.timestamp >= _auction.endTime, "Auction hasn't completed");
+
+    if (_auction.highestBidder == address(0)) {
+      // R.I.P.
+      eipandas.safeTransferFrom(
+        address(this),
+        address(0x000000000000000000000000000000000000dEaD),
+        _auction.pandaId
+      );
+      emit PandaKilled(_auction.pandaId);
+    } else {
+      // Transfer the panda to the highest bidder
+      eipandas.safeTransferFrom(address(this), _auction.highestBidder, _auction.pandaId);
+      emit AuctionSettled(_auction.pandaId, _auction.highestBidder, _auction.highestBid);
+
+      // TODO:
+      // withdraw the stETH from lido contract
+      // will need to do an approve first - from stETH to lido withdraw contract
+
+      // then withdraw - calling requestWithdrawals on lido contract
+      // then transfer the stETH to the highest bidder - we will need the requestId for this?
     }
 
-    modifier noActiveAuction() {
-        require(!auctionInProgress, "An auction is already in progress");
-        _;
-    }
-
-    /**
-     * @notice Settle the current auction, fetch a new panda, and put it up for auction.
-     */
-    function settleCurrentAndCreateNewAuction() external nonReentrant activeAuction {
-        _settleAuction();
-        _startAuction();
-    }
-
-    /**
-     * @notice Place a bid to save a Panda, with a given amount.
-     * @dev This contract only accepts payment in stETH.
-     */
-
-    function placeBid(uint256 _bidAmount) external nonReentrant activeAuction{
-        Auction memory _auction = auction;
-
-        require(block.timestamp < _auction.endTime, 'Auction expired');
-        require(_bidAmount >= reservePrice, 'Must send at least reservePrice');
-        require(
-            _bidAmount >= _auction.highestBid + ((_auction.highestBid * minBidIncrementPercentage) / 100),
-            'Must send more than last bid by minBidIncrementPercentage amount'
-        );
-
-        // Transfer the stETH tokens from the bidder to the contract
-        require(lidoStETH.transferFrom(msg.sender, address(this), _bidAmount), "Insufficent stETH or approval not set");
-
-        address lastBidder_ = _auction.highestBidder;
-
-        // Refund the last bidder, if applicable
-        if (lastBidder_ != address(0)) {
-            // Transfer the previous highest bid back to the bidder
-            lidoStETH.transfer(lastBidder_, _auction.highestBid);
-        }
-
-        auction.highestBid = _bidAmount;
-        auction.highestBidder = msg.sender;
-
-        // Extend the auction if the bid was received within `timeBuffer` of the auction end time
-        bool extended = _auction.endTime - block.timestamp < timeBuffer;
-        if (extended) {
-            auction.endTime = _auction.endTime = block.timestamp + timeBuffer;
-        }
-
-        emit AuctionBid(_auction.pandaId, auction.highestBidder, auction.highestBid, extended);
-
-        if (extended) {
-            emit AuctionExtended(_auction.pandaId, _auction.endTime);
-        }
-    }
-
-    /**
-     * @notice Set the auction time buffer.
-     * @dev Only callable by the owner.
-     */
-    function setTimeBuffer(uint256 _timeBuffer) external onlyOwner {
-        timeBuffer = _timeBuffer;
-
-        emit AuctionTimeBufferUpdated(_timeBuffer);
-    }
-
-    /**
-     * @notice Set the auction reserve price.
-     * @dev Only callable by the owner.
-     */
-    function setReservePrice(uint256 _reservePrice) external onlyOwner {
-        reservePrice = _reservePrice;
-
-        emit AuctionReservePriceUpdated(_reservePrice);
-    }
-
-    /**
-     * @notice Set the auction minimum bid increment percentage.
-     * @dev Only callable by the owner.
-     */
-    function setMinBidIncrementPercentage(uint8 _minBidIncrementPercentage) external onlyOwner {
-        minBidIncrementPercentage = _minBidIncrementPercentage;
-
-        emit AuctionMinBidIncrementPercentageUpdated(_minBidIncrementPercentage);
-    }
-
-    /**
-     * @notice Set the auction duration time.
-     * @dev Only callable by the owner.
-     */
-    function setDuration(uint256 _duration) external onlyOwner {
-        duration = _duration;
-    }
-
-    function pandasToSave() external view returns (uint256) {
-        return pandasToAuction.length;
-    }
-
-    function depositPandas(uint256[] calldata _tokenIds) external nonReentrant {
-        for (uint256 i = 0; i < _tokenIds.length; i++) {
-            _depositPanda(_tokenIds[i]);
-        }
-    }
-
-    function _depositPanda(uint256 _tokenId) internal {
-        require(eipandas.ownerOf(_tokenId) == msg.sender, "Ser, this is not your panda");
-        eipandas.safeTransferFrom(msg.sender, address(this), _tokenId);
-        pandasToAuction.push(_tokenId);
-        emit PandaDeposited(_tokenId);
-    }
-
-    /**
-     * @notice Create an auction.
-     * @dev Store the auction details in the `auction` state variable and emit an AuctionCreated event.
-     * Check to make sure there are pandas to auction. Start new auction with panda using LIFO
-     */
-    function _startAuction() internal nonReentrant noActiveAuction {
-        // check if there are any pandas to auction
-        uint256 numPandasToAuction_ = pandasToAuction.length;
-        require(numPandasToAuction_ > 0, "No pandas to save at this time");
-
-        // grab a panda from the array using LIFO
-        uint256 pandaId_ = pandasToAuction[numPandasToAuction_ - 1];
-        pandasToAuction.pop();
-
-        uint256 startTime_ = block.timestamp;
-        uint256 endTime_ = startTime_ + duration;
-
-        auction = Auction({
-            pandaId: pandaId_,
-            startTime: startTime_,
-            endTime: endTime_,
-            highestBid: 0,
-            highestBidder: address(0)
-        });
-
-        auctionInProgress = true;
-
-        emit AuctionCreated(pandaId_, startTime_, endTime_);
-    }
-
-    /**
-     * @notice Settle an auction, finalizing the bid and paying out to the owner.
-     * @dev If there are no bids, the Panda is burned.
-     */
-    function _settleAuction() internal nonReentrant activeAuction {
-        Auction memory _auction = auction;
-
-        require(block.timestamp >= _auction.endTime, "Auction hasn't completed");
-
-
-        if (_auction.highestBidder == address(0)) {
-            // R.I.P.
-            eipandas.safeTransferFrom(address(this), address(0x000000000000000000000000000000000000dEaD), _auction.pandaId);
-            emit PandaKilled(_auction.pandaId);
-        } else {
-            // Transfer the panda to the highest bidder
-            eipandas.safeTransferFrom(address(this), _auction.highestBidder, _auction.pandaId);
-            emit AuctionSettled(_auction.pandaId, _auction.highestBidder, _auction.highestBid);
-
-            // TODO:
-            // withdraw the stETH from lido contract
-            // will need to do an approve first - from stETH to lido withdraw contract
-
-            // then withdraw - calling requestWithdrawals on lido contract
-            // then transfer the stETH to the highest bidder - we will need the requestId for this?
-
-        }
-
-        auctionInProgress = false;
-    }
+    auctionInProgress = false;
+  }
 }
